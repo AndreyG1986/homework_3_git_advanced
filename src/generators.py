8cc0dad0b96f0dc29d65a2cf8947df15d210b286
Andrey Gorlov <andgor86@yandex.ru>
2025-05-03 19:30:08 +0300

    add generators

---
+++ src/generators.py
@@ -0,0 +1,121 @@
+transactions = (
+    [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+)
+
+
+
+def filter_by_currency(transaction_list, currency):
+    sorted_by_currency_list = iter(filter(lambda x: x['operationAmount']['currency']['code'] == currency, transaction_list))
+
+    return sorted_by_currency_list
+
+if __name__ == "__main__":
+    usd_transactions = filter_by_currency(transactions, "USD")
+    for _ in range(2):
+        print(next(usd_transactions))
+
+
+def transaction_descriptions(transaction_list):
+    for transaction in transaction_list:
+
+        yield transaction["description"]
+
+
+if __name__ == "__main__":
+    descriptions = transaction_descriptions(transactions)
+    for _ in range(5):
+        print(next(descriptions))
+
+
+def card_number_generator(start,end):
+    template = "0000 0000 0000 000"
+    generated_card_numbers = [template + str(n) for n in range(start,end)]
+    for card_number in generated_card_numbers:
+
+        yield card_number
+
+
+if __name__ == "__main__":
+    generator = card_number_generator(4,8)
+    print(next(generator))
+    print(next(generator))
+    print(next(generator))
+    print(next(generator))
+
+
