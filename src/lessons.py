ad38473babe71438d9f01d8149ffa4af02d85600
Andrey Gorlov <andgor86@yandex.ru>
2025-09-23 18:54:50 +0300

    upd lessons & conftest

--- src/lessons.py
+++ src/lessons.py
@@ -1,15 +1,17 @@
 from functools import reduce, wraps
 from time import perf_counter, sleep
 
+
 def timeit(func):
     @wraps(func)
-    def wrapper(*args,**kwargs):
+    def wrapper(*args, **kwargs):
         start = perf_counter()
-        result = func(*args,**kwargs)
+        result = func(*args, **kwargs)
         stop = perf_counter()
         result_time = stop - start
         print(f"{func.__name__} выполнилась за {result_time: 5f}")
         return result
+
     return wrapper
 
 
@@ -21,28 +23,35 @@ def slowit(seconds):
             sleep(seconds)
 
             return result
+
         return wrapper
+
     return inner
+
 
 # Без кеширования время работы функции при каждом вызове не менее 2 секунд.
 @timeit
 @slowit(2)
 def product(n):
-    return reduce(lambda x, y: x * y, range(1, n+1)) if n > 0 else None
+    return reduce(lambda x, y: x * y, range(1, n + 1)) if n > 0 else None
+
 
 print(product(10))
 print(product(10))
+
 
 def memoize(func):
     cash = {}
+
     @wraps(func)
-    def wrapper(*args,**kwargs):
+    def wrapper(*args, **kwargs):
         if args not in cash:
             result = func(*args, **kwargs)
             cash[args] = result
         else:
-            result =cash[args]
-        return  result
+            result = cash[args]
+        return result
+
     return wrapper
 
 
@@ -51,7 +60,8 @@ def memoize(func):
 @memoize
 @slowit(2)
 def product(n):
-    return reduce(lambda x, y: x * y, range(1, n+1)) if n > 0 else None
+    return reduce(lambda x, y: x * y, range(1, n + 1)) if n > 0 else None
+
 
 print(product(10))
 print(product(10))
